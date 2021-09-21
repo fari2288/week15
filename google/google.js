e4d2c74042837876e4f971487bfe744ef2d5db39
Fariza Kubanova <farizatkubanova@gmail.com>
2021-09-21 13:36:12 +0300

    login

---
+++ google/google.js
@@ -0,0 +1,25 @@
+function check() {
+let email = document.getElementById ("exampleemail");
+let name = document.getElementById("examplename");
+let surname = document.getElementById("examplesurname");
+let password = document.getElementById('examplepassword');
+let confirm = document.getElementById('exampleconfirm');
+
+
+document.getElementById('errorMessage').innerHTML ="";
+if (email.value =='') {
+    document.getElementById('errorMessage').innerHTML +="Ваш email не заполнен<br>";
+}
+if (name.value =='') {
+    document.getElementById('errorMessage').innerHTML +="Ваш email не заполнен<br>";
+}
+if (surname.value =='') {
+    document.getElementById('errorMessage').innerHTML +="Ваш email не заполнен<br>";
+}
+if (password.value =='') {
+    document.getElementById('errorMessage').innerHTML +="Ваш email не заполнен<br>";
+}
+if (confirm.value =='') {
+    document.getElementById('errorMessage').innerHTML +="Ваш email не заполнен<br>";
+}
+}
