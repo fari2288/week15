edc0005e5c94497c4218885f66c835dedcb8f577
Fariza Kubanova <farizatkubanova@gmail.com>
2021-09-29 19:22:14 +0100

    доделала

--- google/google.js
+++ google/google.js
@@ -4,26 +4,26 @@ let name = document.getElementById("examplename");
 let surname = document.getElementById("examplesurname");
 let password = document.getElementById('examplepassword');
 let confirm = document.getElementById('exampleconfirm');
+let errorMessage = document.getElementById('errorMessage');
 
 
 document.getElementById('errorMessage').innerHTML ="";
 if (email.value =='') {
-    document.getElementById('errorMessage').innerHTML +="Ваш емейл не заполнен<br>";
+    errorMessage.innerHTML +="Ваш емейл не заполнен<br>";
 }
 if (name.value =='') {
-    document.getElementById('errorMessage').innerHTML +="Вашe имя не заполнено<br>";
+    errorMessage.innerHTML +="Вашe имя не заполнено<br>";
 }
 if (surname.value =='') {
-    document.getElementById('errorMessage').innerHTML +="Ваша фамилия не заполнена<br>";
+    errorMessage.innerHTML +="Ваша фамилия не заполнена<br>";
 }
 if (password.value =='') {
-    document.getElementById('errorMessage').innerHTML +="Ваш пароль не заполнен<br>";
+    errorMessage.innerHTML +="Ваш пароль не заполнен<br>";
 }
 if (confirm.value =='') {
-    document.getElementById('errorMessage').innerHTML +="Подтверждение пароля не заполнено<br>";
+    errorMessage.innerHTML +="Подтверждение пароля не заполнено<br>";
 }
 else {
-    
-    document.getElementById('errorMessage').innerHTML +=`Добро пожаловать, ` + name.value + `!`;
+    errorMessage.innerHTML +=`Добро пожаловать, ${name.value}!`;
 }
 }
